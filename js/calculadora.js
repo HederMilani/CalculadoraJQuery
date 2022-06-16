500f17f818a7a312460efffe9d121cc179bedf2e
Heder Milani <hedermilani@hotmail.com>
2022-06-16 16:54:57 -0300

    Chamar Operações

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,10 +1,12 @@
-//const { event } = require("jquery");
+import "./operacoes"
 
-var num1;
-var num2;
+var valor1;
+var valor2;
+var resultado;
 var operacao;
 
 $(document).ready(function(){
+    //Atribuir valores
     $('#num9').click(function(){
         valor(event, 9);
     });
@@ -38,11 +40,57 @@ $(document).ready(function(){
     $('#num00').click(function(){
         valor(event, '00');
     });
+    $('#virgula').click(function(){
+        valor(event, '.');
+    });
 
+    //Chamar Operações
+    $('#zerar').click(function(event){
+        event.preventDefault();
+        zerar();
+    });
+    $('#inverter').click(function(event){
+        event.preventDefault();
+        $('#result').val(parseInt($('#result').val()) * (0 - 1));
+    });
+    $('#dividir').click(function(event){
+        event.preventDefault();
+        valor1 = parseInt($('#result').val())
+        operacao = 'dividir';
+        zerar();
+    });
+    $('#subtratir').click(function(event){
+        event.preventDefault();
+        valor1 = parseInt($('#result').val())
+        operacao = 'subtrair';
+        zerar();
+    });
+    $('#multiplicar').click(function(event){
+        event.preventDefault();
+        valor1 = parseInt($('#result').val())
+        operacao = 'multiplicar';
+        zerar();
+    });
+    $('#somar').click(function(event){
+        event.preventDefault();
+        valor1 = parseInt($('#result').val())
+        operacao = 'somar';
+        zerar();
+    });
+    $('#igual').click(function(event){
+        event.preventDefault();
+        valor2 = parseInt($('#resut').val);
+        
+    })
+
+    //Tratamento de valores
     function valor(event, num) {
         event.preventDefault();
         console.log(num);
         $('#result').val($('#result').val() + num);
+    }
+    function zerar (){
+        $('#result').val("");
     }
 });
 
---
+++ js/operacoes.js
@@ -0,0 +1,10 @@
+function calculo (valor1, valor2, operacao) {
+    switch (operacao) {
+        case somar:
+            return valor1 + valor2;
+            break;
+    
+        default:
+            break;
+    }
+}
